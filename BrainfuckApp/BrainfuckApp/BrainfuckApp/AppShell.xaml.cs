628d0f45cf9d2f79423c363c643b0ab6cb081def
kurema <kurema_makoto_software@yahoo.co.jp>
2021-09-30 18:00:36 +0900

    Init

---
+++ BrainfuckApp/BrainfuckApp/BrainfuckApp/AppShell.xaml.cs
@@ -0,0 +1,18 @@
+﻿using BrainfuckApp.ViewModels;
+using BrainfuckApp.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace BrainfuckApp
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            //Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+        }
+
+    }
+}
